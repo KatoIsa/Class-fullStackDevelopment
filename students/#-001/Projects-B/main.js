047c1cab6b720ad1a65319b2fd846778bf606a63
CHASHUR <chashurrock@gmail.com>
2024-07-08 14:41:23 +0530

    added documents

---
+++ students/#-001/Projects-B/main.js
@@ -0,0 +1,10 @@
+let search =document.getElementById('search');
+let add = document.querySelectorAll('.button');
+let remove = document.querySelectorAll('.button2')
+let check = document.getElementById('checkbox')
+
+// takeing inputs 
+
+let input = search.value;
+console.log(input);
+
