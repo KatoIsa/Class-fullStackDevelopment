4495b56352e03889c5b2c43698255b50dee104fa
CHASHUR <chashurrock@gmail.com>
2024-07-08 18:03:44 +0530

    created every thing as planned

--- students/#-001/Projects-B/main.js
+++ students/#-001/Projects-B/main.js
@@ -1,15 +1,54 @@
-let search =document.getElementById('search');
-let insert = document.querySelectorAll('.button');
-let remove = document.querySelectorAll('.button2')
-let check = document.getElementById('checkbox')
+/** @format */
 
-// takeing inputs 
-insert.forEach(button=>{
+let search = document.getElementById("search");
+let insert = document.querySelectorAll(".button");
+let check = document.querySelector(".checkbox");
 
-    button.addEventListener('click',()=>{
-        
-        let input = search.value;
-            console.log(input);
-    })
-})
-    
+// Add event listeners to each "ADD" button
+insert.forEach((button) => {
+	button.addEventListener("click", () => {
+		let input = search.value;
+
+		// createing div
+		let div = document.createElement("div");
+
+		// Create label element
+		let label = document.createElement("label");
+		label.textContent = input;
+		label.className = "label";
+
+		// Create checkbox element
+		let checkbox = document.createElement("input");
+		checkbox.type = "checkbox";
+        checkbox.className= "checkbox";
+        checkbox.style.width='20px';
+        checkbox.style.height='20px';
+
+		// Create delete button element
+		let del = document.createElement("button");
+		del.textContent = "DELETE";
+		del.className = "button2";
+
+		// Append elements to the div container
+		div.appendChild(label);
+		div.appendChild(checkbox);
+		div.appendChild(del);
+
+		// Append the div to the checkbox container
+		check.appendChild(div);
+
+		
+		checkbox.addEventListener("change", () => {
+			if (checkbox.checked) {
+				label.classList.add("underlined");
+			} else {
+				label.classList.remove("underlined");
+			}
+		});
+
+		
+		del.addEventListener("click", () => {
+			check.removeChild(div);
+		});
+	});
+});
